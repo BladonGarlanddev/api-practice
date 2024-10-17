7e824398378a291108832f93721cf1200764d600
BladonGarlanddev <bladoncgarland@gmail.com>
2024-10-17 17:59:31 -0400

    late commit
    forgot to commit as I go but I added stuff to get a bunch of data, or one thing of it. Stuff to add data to the server, and update it.

--- client.ts
+++ client.ts
@@ -1,4 +1,16 @@
-import axios from 'axios';
+import axios, { Axios, AxiosResponse } from 'axios';
+
+// Getting a resource: https://jsonplaceholder.typicode.com/posts/1
+// Listing all resources: https://jsonplaceholder.typicode.com/posts
+// Creating a resource: https://jsonplaceholder.typicode.com/posts
+// Deleting a resource: https://jsonplaceholder.typicode.com/posts/1
+
+interface Post {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
 
 const axiosInstance = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com/",
@@ -6,3 +18,55 @@ const axiosInstance = axios.create({
   headers: {},
 });
 
+const listAllResources = async ():Promise<Post[]> => {
+    const result:AxiosResponse<Post[]> = await axiosInstance.get<Post[]>('posts')
+    return result.data
+}
+
+const getAllTitles = async ():Promise<string[]> => {
+    const result:Post[] = await listAllResources()
+    let output:string[] = []
+
+    for (let object of result) {
+        output.push(object.title)
+    }
+
+    return output
+}
+
+const getUserPosts = async (userId:number):Promise<Post[]> => {
+    const result:Post[] = await listAllResources()
+    let output:Post[] = []
+    
+    for (let post of result) {
+        if (post.userId == userId) {
+            output.push(post)
+        }
+    }
+
+    return output
+}
+
+const addResource = async (data:Post):Promise<{status:number, data:Post}> => {
+    let result:AxiosResponse<Post> = await axiosInstance.post('posts', data)
+    return {status:result.status, data:result.data}
+}
+
+const updateResource = async(data:Post, postId:number):Promise<{status: number, data:Post}> => {
+    let result:AxiosResponse<Post> = await axiosInstance.put(`posts/${postId}`, data)
+    return {status: result.status, data: result.data}
+}
+
+
+async function main() {
+    let data:Post = {
+        userId: 101,
+        id: 101,
+        title: "hairy balls",
+        body: "Big hairy balls"
+    }
+    let result = await updateResource(data, 1)
+    console.log(result)
+}
+
+main()
